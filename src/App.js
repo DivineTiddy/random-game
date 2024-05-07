a649052d1ee94a84428e03879450c8e42c7c9065
Divine <divinetidy@gmail.com>
2024-05-07 21:14:36 +0100

    new game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+//const hiddenNum = Math.trunc(Math.random() * 10) + 1;
+//console.log(hiddenNum);
+
+export default function App() {
+  const [showresult, setShowresult] = useState(10);
+  const [viewWin, setViewWin] = useState();
+  const [input, setInput] = useState(0);
+  const [hiddenNum, sethiddenNum] = useState(0);
+  const [computerPoint, setComputerPoint] = useState(10);
+  const [level, setLesvel] = useState(1);
+  const [select_level, setSelectLevel] = useState(5)
+  const [emoji , setEmoji] = useState(``)
+ 
+  function showValue(e) {
+    const hiddenNum = Math.trunc(Math.random() * select_level) + 1;
+    e.preventDefault();
+
+    if (input > select_level || input < 1) {
+      alert(`Invalid number`);
+    } else {
+      if (input === hiddenNum) {
+        setShowresult((el) => el + 1);
+      } else {
+        setShowresult((el) => el - 1);
+      }
+      if (input === hiddenNum) {
+        setComputerPoint((el) => el - 1);
+      } else {
+        setComputerPoint((el) => el + 1);
+      }
+      if (showresult === 19) {
+        setLesvel((el) => el + 1);
+        setEmoji((el) => `🎉`);
+      }
+     
+      if (computerPoint === 19) {
+        setLesvel((el) => el - 1);
+      }
+      setViewWin(input);
+      sethiddenNum(hiddenNum);
+      
+    }
+  }
+  //  function showresult(showresult)
+  //    console.log(showresult);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appbody">
+      <Restart setSelectLevel={setSelectLevel} setShowresult={setShowresult} setComputerPoint={setComputerPoint}></Restart>
+      <Showresult
+        computerPoint={computerPoint}
+        hiddenNum={hiddenNum}
+        showresult={showresult}
+        viewWin={viewWin}
+      ></Showresult>
+      <InputNumberAndScore
+        hiddenNum={hiddenNum}
+        viewWin={viewWin}
+        showValue={showValue}
+        setInput={setInput}
+        showresult={showresult}
+        computerPoint={computerPoint}
+        level={level}
+        setComputerPoint={setComputerPoint}
+        setShowresult={setShowresult}
+        emoji={emoji}
+        setEmoji={setEmoji}
+      ></InputNumberAndScore>
     </div>
   );
 }
 
-export default App;
+function Restart( {setSelectLevel , setShowresult , setComputerPoint}) {
+  function HandleRestart() {
+    setComputerPoint(10)
+    setShowresult(10)
+    
+  }
+
+  return (
+    <div className="nav">
+      <div onClick={HandleRestart} className="buttons">
+   <button className="btn"><span></span><p data-start="good luck!" data-text="start!" data-title="new game"></p></button>
+</div>
+      <select onChange={e => setSelectLevel(Number(e.target.value))} className="select" >
+        <option value={3}>EASY 1 to 3</option>
+        <option value={5} className="option">MEDIUM 1 to 5</option>
+        <option value={10}>HARD 1 to 10</option>
+      </select>
+    </div>
+  );
+}
+
+function Showresult({ computerPoint, hiddenNum, showresult, viewWin }) {
+  // const display = viewWin === hiddenNum ? hiddenNum : "?";
+  const congrat = viewWin === hiddenNum ? `Won` : `Lost`;
+  return (
+    <div className="showresult">
+      <div className="owner">
+        <p className="style">YOU😎</p>
+        <p className="">{showresult <= 0 ? 0 : showresult >= 20 ? 20 : showresult}</p>
+      </div>
+
+      <div className="">
+        <p className={!viewWin ? "style_two" : viewWin === hiddenNum ? "won" : "lost"}>{viewWin ? congrat : ""}</p>
+        <h3 className={!viewWin ? "style_two" : viewWin === hiddenNum ? "won" : "lost"}>{hiddenNum ? hiddenNum : "?"}</h3>
+      </div>
+
+      <div className="computer">
+        <p className="style">CPU🤖</p>
+        <p className="">
+          {computerPoint <= 0 ? 0 : computerPoint >= 20 ? 20 : computerPoint}
+          
+        </p>
+      </div>
+    </div>
+  );
+}
+function InputNumberAndScore({
+  setComputerPoint,
+  level,
+  computerPoint,
+  showresult,
+  showValue,
+  setInput,
+  setShowresult,
+  emoji,
+  setEmoji
+}) {
+  function restartGame() {
+    setComputerPoint(10);
+    setShowresult(10);
+    setEmoji(``)
+
+  }
+  return (
+    <div className="InputNumberAndScore">
+      {computerPoint === 20 || showresult === 20 ? (
+        <div>
+          <p>level up</p>
+          <button className="continue" onClick={restartGame}>continue</button>
+        </div>
+      ) : (
+        <div>
+          <form onSubmit={showValue}>
+            <input
+              autoFocus
+              onChange={(e) => setInput(Number(e.target.value))}
+              type="text"
+            ></input>
+           <button className="button" type="submit">
+  play
+</button>
+          </form>
+        </div>
+      )}
+
+      <div className="level">
+        <p ><span className="style">🚀level</span>  {level <= 1 ? 1 : level}{emoji}</p>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,206 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 5,
+    photoName: "pizza/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizza/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    // photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    // photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    // photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "/pizza/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to our pizza menu</h1>
+    </header>
+  );
+}
+function Main() {
+  return (
+    <main className="main">
+      {pizzaData.map((pizza) => (
+        <Pizza pizzaObj={pizza} key={pizza.name} />
+      ))}
+    </main>
+  );
+}
+function Footer() {
+
+  const hour = new Date().getHours();
+  const open = 12;
+  const close = 22;
+  const isOpen = hour >= open && hour <= close;
+  return (
+    <footer>
+      {isOpen ? <Order openHour={hour} closeHour={close} /> : `we are close`}
+    </footer>
+  );
+}
+
+function Order({ closeHour, openHour }) {
+ 
+  return (
+    <p>
+      we are currently now {openHour}:00 untill {closeHour}:00 we close
+    </p>
+  );
+}
+function Pizza({pizzaObj}) {
+  return (
+    <div className={`pizza_container ${pizzaObj.soldOut ? `sold_out`:``}`} >
+      <div className="pizza_row">
+        <img
+          className="pizza_photo"
+          src={pizzaObj.photoName}
+          alt={pizzaObj.name}
+        ></img>
+        <div>
+          <h1>{pizzaObj.name}</h1>
+          <p>{pizzaObj.ingredients}</p>
+          <p>{pizzaObj.soldOut ? `Sold out`:<span>
+            {pizzaObj.price}
+           </span>}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const mySkill = [
+  {
+    skill: `javascript`,
+    level: `advance`,
+    color: `#FF5810`,
+  },
+  {
+    skill: `Html`,
+    level: `bigginer`,
+    color: `#60DAFB`,
+  },
+  {
+    skill: `css`,
+    level: `intermidiate`,
+    color: `#FF3800`,
+  },
+  {
+    skill: `react`,
+    level: `advance`,
+    color: `#C3DCAF`,
+  },
+];
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <div className="image">
+          <Avater></Avater>
+        </div>
+        <Text></Text>
+        <div className="skillLevel">
+          <SkillList />
+        </div>
+      </div>
+    </>
+  );
+}
+function Avater() {
+  return (
+    <>
+      <img className="photo" src="\pizza\prosciutto.jpg" alt="pizza"></img>
+    </>
+  );
+}
+function Text() {
+  return (
+    <>
+      <h1>Hello Divine</h1>
+    </>
+  );
+}
+function SkillList() {
+  return (
+    <>
+      {mySkill.map((skill) => (
+        <Skill
+          skill={skill.skill}
+          level={skill.level}
+          color={skill.color}
+          key={skill.color}
+        />
+      ))}
+    </>
+  );
+}
+function Skill({ skill, level, color }) {
+  return (
+    <>
+      <div className="showskill" style={{ background: color }}>
+        <p>{skill}</p>
+        <p>{level}</p>
+      </div>
+    </>
+  );
+}
+*/
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <react.StrictMode>
     <App />
-  </React.StrictMode>
+  </react.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
